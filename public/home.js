37dfcd1b52b680fe4e965851bbb5f5788b54cd61
Ahmed magdy <45017562+ProgrammerAHM@users.noreply.github.com>
2023-04-20 10:07:00 +0200

    Update home.js

--- public/home.js
+++ public/home.js
@@ -35,7 +35,7 @@ function setfolders(lis) {
                 <div class="dropdown-menu w-40" id="_ur6u88q04" data-popper-placement="bottom-end" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 20px);">
                 <ul class="dropdown-content">
                     <li>
-                        <button onclick="getImgs(${lis[i].name},this)" class="btn py-1 px-2 btn-outline-secondary"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="file" data-lucide="file" class="lucide lucide-file w-4 h-4 mr-2"><path d="M14.5 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V7.5L14.5 2z"></path><polyline points="14 2 14 8 20 8"></polyline></svg> Copy to clip </button>
+                        <button onclick="getImgs('${lis[i].name}',this)" class="btn py-1 px-2 btn-outline-secondary"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="file" data-lucide="file" class="lucide lucide-file w-4 h-4 mr-2"><path d="M14.5 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V7.5L14.5 2z"></path><polyline points="14 2 14 8 20 8"></polyline></svg> Copy to clip </button>
                     </li>
                 </ul>
             </div>
